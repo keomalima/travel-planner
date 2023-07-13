19727bf10a2c73596034a757cdf2dd183f12f075
Keoma Lima <keomalima@outlook.com>
2023-07-13 20:46:36 -0300

    FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Headers';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
+    </>
+  );
+};
+
+export default App;
